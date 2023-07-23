e013bb8fc1a15a4ba786e92a5ae385637f078465
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-23 18:31:16 -0500

    CREANDO SERIALIZADOR

---
+++ tarea/api/crud/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from .models import Autor, Libro
+
+class AutorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = '__all__'
+
+class LibroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Libro
+        fields = '__all__'
