fce69d9934db3abedd26dc0d19ce7545d18f80ce
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-23 20:44:27 -0500

    crud

--- tarea/api/crud/urls.py
+++ tarea/api/crud/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AutorViewSet, LibroViewSet
+from .views import *
 
 # Crea un router y registra los ViewSets
 router = DefaultRouter()
@@ -10,4 +10,10 @@ router.register(r'libros', LibroViewSet)
 # Las URLs de la API están determinadas automáticamente por el router
 urlpatterns = [
     path('', include(router.urls)),
+    path('autores/', crear_autor),
+    path('autores/', obtener_lista_autores, name='lista_autores'),
+    path('autores/<int:autor_id>/', obtener_detalle_autor, name='detalle_autor'),
+    path('autores/crear/', crear_autor, name='crear_autor'),
+    path('autores/<int:autor_id>/actualizar/', actualizar_autor, name='actualizar_autor'),
+    path('autores/<int:autor_id>/eliminar/', eliminar_autor, name='eliminar_autor'),
 ]
--- tarea/api/crud/views.py
+++ tarea/api/crud/views.py
@@ -1,6 +1,56 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Autor, Libro
 from .serializers import AutorSerializer, LibroSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def crear_autor(request):
+    if request.method == 'POST':
+        serializer = AutorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET'])
+def obtener_lista_autores(request):
+    if request.method == 'GET':
+        autores = Autor.objects.all()
+        serializer = AutorSerializer(autores, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET'])
+def obtener_detalle_autor(request, autor_id):
+    try:
+        autor = Autor.objects.get(id=autor_id)
+    except Autor.DoesNotExist:
+        return Response({"error": "El autor no existe."}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AutorSerializer(autor)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['PUT'])
+def actualizar_autor(request, autor_id):
+    try:
+        autor = Autor.objects.get(id=autor_id)
+    except Autor.DoesNotExist:
+        return Response({"error": "El autor no existe."}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = AutorSerializer(autor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['DELETE'])
+def eliminar_autor(request, autor_id):
+    try:
+        autor = Autor.objects.get(id=autor_id)
+    except Autor.DoesNotExist:
+        return Response({"error": "El autor no existe."}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        autor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class AutorViewSet(viewsets.ModelViewSet):
     queryset = Autor.objects.all()
