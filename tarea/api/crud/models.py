d7c143765448edb1862095dab8e08a9bd8e9c855
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-23 19:52:54 -0500

    modificaciones

--- tarea/api/crud/models.py
+++ tarea/api/crud/models.py
@@ -4,17 +4,15 @@ from django.db import models
 
 class Autor(models.Model):
     nombre = models.CharField(max_length=100)
-    nacionalidad = models.CharField(max_length=50)
-    fecha_nacimiento = models.DateField()
+    pais = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre
 
 class Libro(models.Model):
-    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=100)
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
-    editorial = models.CharField(max_length=100)
-    fecha_publicacion = models.DateField()
+    descripcion = models.TextField()
 
     def __str__(self):
         return self.titulo
--- tarea/api/crud/serializers.py
+++ tarea/api/crud/serializers.py
@@ -1,13 +1,12 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Autor, Libro
 
-class AutorSerializer(serializers.HyperlinkedModelSerializer):
+class AutorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Autor
         fields = '__all__'
 
-class LibroSerializer(serializers.HyperlinkedModelSerializer):
+class LibroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Libro
         fields = '__all__'
